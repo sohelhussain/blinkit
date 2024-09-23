1357f26c69c6b1963f5d81fa5f343a06fe2f3f2c
sohelhussain <sohelhussaing@gmail.com>
2024-09-23 15:14:16 +0530

    feat: ADDING YAML & MD FILE & REWRITE A PRODUCT.JS CODE IN ROUTER

--- router/product.js
+++ router/product.js
@@ -9,6 +9,60 @@ const { validateAdmin, userIsLoggedIn } = require('../middlewares/admin');
 const streamUpload = require('../utils/streamUpload'); // Utility function for Cloudinary upload
 const redisClient = require('../config/redisClient'); // Import Redis client from config
 
+router.get("/", userIsLoggedIn, async function (req, res) {
+  let somethingInCart = false;
+  const resultArray = await productModel.aggregate([
+    {
+      $group: {
+        _id: "$category",
+        products: { $push: "$$ROOT" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        category: "$_id",
+        products: { $slice: ["$products", 10] },
+      },
+    },
+  ]);
+
+  let cart = await cartModel.findOne({ user: req.session.passport.user });
+
+  if (cart && cart.products.length > 0) somethingInCart = true;
+
+  let rnproducts = await productModel.aggregate([{ $sample: { size: 3 } }]);
+
+  // Convert the array into an object
+  const resultObject = resultArray.reduce((acc, item) => {
+    acc[item.category] = item.products;
+    return acc;
+  }, {});
+  res.render("index", {
+    products: resultObject,
+    rnproducts,
+    somethingInCart,
+    cartCount: cart ? cart.products.length : 0,
+  });
+});
+
+router.get("/delete/:id", validateAdmin, async function (req, res) {
+  if (req.user.admin) {
+    let prods = await productModel.findOneAndDelete({ _id: req.params.id });
+    return res.redirect("/admin/products");
+  }
+  res.send("you are not allowed to delete this product.");
+});
+
+router.post("/delete", validateAdmin, async function (req, res) {
+  if (req.user.admin) {
+    let prods = await productModel.findOneAndDelete({
+      _id: req.body.product_id,
+    });
+    return res.redirect("back");
+  }
+  res.send("you are not allowed to delete this product.");
+});
 
 // Create a new product
 router.post('/', upload.single('image'), async (req, res) => {
@@ -71,9 +125,5 @@ router.post('/', upload.single('image'), async (req, res) => {
     res.send(error.message);
   }
 });
-
-// router.get('/', async (req, res) =>{
-//   res.render('products');
-// })
 
 module.exports = router;
